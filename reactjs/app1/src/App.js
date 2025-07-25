41722a9537b84255c5e0f6d3b8bfd66cb94a57cd
theeasylearn <theeasylearn@gmail.com>
2025-07-25 17:07:19 +0530

    app.js

--- reactjs/app1/src/App.js
+++ reactjs/app1/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
+//functional components
 function App() {
   return (
     <div className="App">
--- reactjs/app1/src/index.js
+++ reactjs/app1/src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+//create virtual DOM
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+let num1 = 10; 
+let num2 = 3;
+let name = "the easylearn academy";
+let output = (<div>
+    <h1>{name}</h1>
+    <table border='1' align='center' cellpadding='10'>
+        <tr>
+            <td>Addition</td>
+            <td>{num1 + num2}</td>
+        </tr>
+        <tr>
+            <td>Subtraction</td>
+            <td>{num1 - num2}</td>
+        </tr>
+        <tr>
+            <td>Multiplication</td>
+            <td>{num1 * num2}</td>
+        </tr>
+        <tr>
+            <td>Division</td>
+            <td>{num1 / num2}</td>
+        </tr>
+        
+    </table>
+</div>)
+root.render(output);
