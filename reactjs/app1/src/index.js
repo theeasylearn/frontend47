b919ad9137cf91f371143f62ab032fcc9cf16cee
theeasylearn <theeasylearn@gmail.com>
2025-07-26 17:04:20 +0530

    index

--- reactjs/app1/src/index.js
+++ reactjs/app1/src/index.js
@@ -1,31 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
 //create virtual DOM
 const root = ReactDOM.createRoot(document.getElementById('root'));
-let num1 = 10; 
-let num2 = 3;
-let name = "the easylearn academy";
-let output = (<div>
-    <h1>{name}</h1>
-    <table border='1' align='center' cellpadding='10'>
-        <tr>
-            <td>Addition</td>
-            <td>{num1 + num2}</td>
-        </tr>
-        <tr>
-            <td>Subtraction</td>
-            <td>{num1 - num2}</td>
-        </tr>
-        <tr>
-            <td>Multiplication</td>
-            <td>{num1 * num2}</td>
-        </tr>
-        <tr>
-            <td>Division</td>
-            <td>{num1 / num2}</td>
-        </tr>
-        
-    </table>
+let page = (<div className="container">
+    <div className="row mt-5">
+        <div className="col-6 offset-3">
+            <div className="card shadow">
+                <div className="card-header text-bg-primary">
+                    <h3>Register</h3>
+                </div>
+                <div className="card-body">
+                    <form action="" method="post">
+                        {/* form */}
+                        <div className="mb-3">
+                            <label htmlFor="email" className="form-label">Email</label>
+                            <input type="email" id="email" name="email" className="form-control" />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="mobile" className="form-label">Mobile</label>
+                            <input type="text" id="mobile" name="mobile" className="form-control" />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="password" className="form-label">Password</label>
+                            <input type="password" id="password" name="password" className="form-control" />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="confirm_password" className="form-label">Confirm Password</label>
+                            <input type="password" id="confirm_password" name="confirm_password"
+                                className="form-control" />
+                        </div>
+                        <div className="text-end">
+                            <button type="submit" className="btn btn-primary">Let's Do</button>
+                            <button type="reset" className="btn btn-dark">Clear all</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>)
-root.render(output);
+root.render(page);
