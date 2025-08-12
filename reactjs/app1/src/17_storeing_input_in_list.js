a0fd9b243998031e87101b0e1b3673e0a586dc40
theeasylearn <theeasylearn@gmail.com>
2025-08-12 18:43:57 +0530

    index js

--- reactjs/app1/src/17_storeing_input_in_list.js
+++ reactjs/app1/src/17_storeing_input_in_list.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import "bootstrap/dist/css/bootstrap.min.css";
-import Resturant from './components/resturant_2';
+import Instagram from './components/instagram';
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Resturant />)
+root.render(<Instagram />);
