fd4b541448ce8993cbad24e244f0ca0f78a63321
theeasylearn <theeasylearn@gmail.com>
2025-08-19 17:08:09 +0530

    event handler

--- reactjs/app1/src/18_event_handler_with_argument.js
+++ reactjs/app1/src/18_event_handler_with_argument.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import "bootstrap/dist/css/bootstrap.min.css";
-import Menu from './components/menu';
+import Scoreboard from './components/scoreboard';
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Menu />)
+root.render(<Scoreboard />)
