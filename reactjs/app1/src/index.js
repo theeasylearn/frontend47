095c29264aa4b85b1cd6c82d3f59c9ed671be625
theeasylearn <theeasylearn@gmail.com>
2025-08-27 17:03:03 +0530

    routing

--- reactjs/app1/src/index.js
+++ reactjs/app1/src/index.js
@@ -1,4 +1,11 @@
 import ReactDOM from 'react-dom/client';
-import Shop from './components/shop';
+import Home from './components/home';
+import Aboutus from './components/aboutus';
+import Product from './components/products';
+import Service from './components/services';
+import Contactus from './components/contactus';
+import './site.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Shop />);
+root.render(<Contactus />);
