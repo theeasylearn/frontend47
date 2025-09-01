ed321dac08dccf23ab22bbb01da4c4ef870c4c59
theeasylearn <theeasylearn@gmail.com>
2025-09-01 16:08:15 +0530

    test

--- reactjs/app1/src/index.js
+++ reactjs/app1/src/index.js
@@ -1,61 +1,89 @@
 import ReactDOM from 'react-dom/client';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useEffect, useState } from 'react';
-function ApiCallingExample() {
-    let [post,setPosts] = useState([]); //create empty state array
-    useEffect(() => {
+import React from 'react';
+class ApiCallingExample2 extends React.Component {
+    constructor(props) {
+        super(props);
+        //create state 
+        this.state = {
+            post: []
+        };
+    }
+    componentDidMount() {
         let apiAddress = "https://jsonplaceholder.typicode.com/posts";
         fetch(apiAddress)
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data);
-            //store data into state array
-            setPosts(data);
-        })
-        .catch((error) => {
-            console.log(error);
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                //store data into state array
+                this.setState({
+                    post: data
+                });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+    display = (item) => {
+        return (<tr>
+            <td>{item.userId}</td>
+            <td>{item.id}</td>
+            <td>
+                {item.title}
+            </td>
+            <td className="pre-line">
+                {item.body}
+            </td>
+            <td>
+                <button className='btn btn-danger' onClick={() => this.deletePost(item.id)}>Trash</button>
+            </td>
+        </tr>);
+    }
+    deletePost = (postID) => {
+        let filteredPosts = this.state.post.filter((item) => {
+            if (item.id !== postID)
+                return item;
         });
-    });
-
-    return (<div className="container">
-        <div className="row mt-5">
-            <div className="col-12">
-                <div className="card shadow">
-                    <div className="card-header">
-                        <h3>API calling using fetch Method</h3>
-                    </div>
-                    <div className="card-body">
-                        <div className="table-container">
-                            <div className="table-responsive">
-                                <table className="table table-hover table-striped align-middle">
-                                    <thead>
-                                        <tr>
-                                            <th scope="col">userId</th>
-                                            <th scope="col">id</th>
-                                            <th scope="col">title</th>
-                                            <th scope="col">body</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <td>1</td>
-                                            <td>1</td>
-                                            <td>
-                                                sunt aut facere
-                                            </td>
-                                            <td className="pre-line">
-                                                quia et suscipit
-                                            </td></tr>
-                                    </tbody>
-                                </table>
+        this.setState({
+            post: filteredPosts
+        });
+    }
+    render() {
+        return (<div className="container">
+            <div className="row mt-5">
+                <div className="col-12">
+                    <div className="card shadow">
+                        <div className="card-header">
+                            <h3>API calling using fetch Method</h3>
+                        </div>
+                        <div className="card-body">
+                            <div className="table-container">
+                                <div className="table-responsive">
+                                    <table className="table table-hover table-striped align-middle">
+                                        <thead>
+                                            <tr>
+                                                <th scope="col">userId</th>
+                                                <th scope="col">id</th>
+                                                <th scope="col">title</th>
+                                                <th scope="col">body</th>
+                                                <th scope='col'>Action</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            {this.state.post.map((item) => this.display(item))}
+                                        </tbody>
+                                    </table>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    )
+        );
+    }
 }
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<ApiCallingExample />);
+root.render(<ApiCallingExample2 />);
