03730a5672a986b38c9de6061243d650fa546e38
theeasylearn <theeasylearn@gmail.com>
2025-09-05 16:37:51 +0530

    Routing

--- reactjs/admin/src/index.js
+++ reactjs/admin/src/index.js
@@ -1,20 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import AdminForgotPassword from './AdminForgotPassword';
+import AdminChangePassword from './AdminChangePassword';
+import AdminLogin from './AdminLogin';
 import AdminAddCategory from './AdminAddCategory';
 import AdminAddProduct from './AdminAddProduct';
 import AdminCategory from './AdminCategory';
-import AdminChangePassword from './AdminChangePassword';
-import AdminDashBoard from './AdminDashboard';
+import AdminDashboard from './AdminDashboard';
 import AdminEditCategory from './AdminEditCategory';
 import AdminEditProduct from './AdminEditProduct';
-import AdminForgotPassword from './AdminForgotPassword';
-import AdminLogin from './AdminLogin';
 import AdminOrder from './AdminOrder';
-import AdminProductOrder from './AdminPrintOrder';
+import AdminPrintOrder from './AdminPrintOrder';
 import AdminProduct from './AdminProduct';
 import AdminUser from './AdminUser';
 import AdminViewProduct from './AdminViewProduct';
 import AdminViewOrder from './AdminViewOrder';
+import AdminPageNotFound from './AdminPageNotFound.js';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function MyRouter() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index path="/" element={<AdminLogin />} />
+        <Route path="/forgot-password" element={<AdminForgotPassword />} />
+        <Route path="/change-password" element={<AdminChangePassword />} />
+        <Route path="/dashboard" element={<AdminDashboard />} />
+        <Route path="/category" element={<AdminCategory />} />
+        <Route path="/add-category" element={<AdminAddCategory />} />
+        <Route path="/edit-category/:id" element={<AdminEditCategory />} />
+        <Route path="/product" element={<AdminProduct />} />
+        <Route path="/add-product" element={<AdminAddProduct />} />
+        <Route path="/edit-product/:id" element={<AdminEditProduct />} />
+        <Route path="/view-product/:id" element={<AdminViewProduct />} />
+        <Route path="/order" element={<AdminOrder />} />
+        <Route path="/view-order/:id" element={<AdminViewOrder />} />
+        <Route path="/print-order/:id" element={<AdminPrintOrder />} />
+        <Route path="/users" element={<AdminUser />} />
+        {/* define 404 route which handle request for non existing routes */}
+        <Route path='*' element={<AdminPageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<AdminViewOrder />);
+root.render(<MyRouter />);
